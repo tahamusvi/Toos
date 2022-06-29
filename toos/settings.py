19a46278ca9d7f01cb840e487ef08b9b93ba8501
Taha <TahaM000@gmail.com>
2022-06-29 11:36:25 +0430

    postgresql

--- toos/settings.py
+++ toos/settings.py
@@ -123,26 +123,26 @@ WSGI_APPLICATION = 'toos.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # SQLITE
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # PSG
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'farline_postgresql',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbpost',
+        'USER': 'postgres',
+        'PASSWORD': 'hNYTlIKOs2uWhNdiLBJpkVRvVKLAccAU',
+        'HOST': 'farlineedu',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
